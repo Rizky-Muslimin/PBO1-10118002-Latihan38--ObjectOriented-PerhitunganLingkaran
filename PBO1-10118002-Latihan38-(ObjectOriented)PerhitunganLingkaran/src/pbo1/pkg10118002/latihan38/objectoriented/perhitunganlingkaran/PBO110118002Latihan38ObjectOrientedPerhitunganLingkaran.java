11cac3a74f12b5941e6ac3aabb7535e52264940c
Rizky Muslimin <55826921+Rizky-Muslimin@users.noreply.github.com>
2019-10-17 21:01:27 +0700

    Add files via upload

---
+++ PBO1-10118002-Latihan38-(ObjectOriented)PerhitunganLingkaran/src/pbo1/pkg10118002/latihan38/objectoriented/perhitunganlingkaran/PBO110118002Latihan38ObjectOrientedPerhitunganLingkaran.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118002.latihan38.objectoriented.perhitunganlingkaran;
+
+/**
+ *
+ * @author 
+ * NAMA     : Rizky Muslimin
+ * KELAS    : IF-1
+ * NIM      : 10118002
+ * Deskripsi Program : Program ini untuk menampilkan tampilan program 
+ *                     perhitungan lingkaran.
+ */
+public class PBO110118002Latihan38ObjectOrientedPerhitunganLingkaran {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    Lingkaran ling = new Lingkaran();
+
+        System.out.println("=======Perhitungan Lingkaran=======");
+        ling.validasiInput();
+        ling.hasilPerhitungan(ling.diameter);
+    }
+    
+}
+
+
+
